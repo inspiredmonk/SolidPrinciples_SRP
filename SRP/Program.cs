96a3816f2f1dea14213f2f05b9b1830986837e7b
Himanshu Jain <himanshu.jain@milliman.com>
2025-07-11 16:06:56 +0530

    Add SRP

---
+++ SRP/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace SRP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ValidateInput validateInput = new ValidateInput();
+            DatabaseOperations databaseOperations = new DatabaseOperations();
+            EmailService emailService = new EmailService();
+
+            UserManager userManager = new UserManager(validateInput, databaseOperations, emailService);
+            userManager.RegisterUser("testUser", "testpassword");
+        }
+    }
+
+    public interface IValidateInput
+    {
+        public bool validateUserInput(string username, string password);
+    }
+
+    public interface IDatabaseOperations
+    {
+        public bool SaveInput();
+    }
+
+    public interface IEmailService
+    {
+        public void sendEmail();
+    }
+
+    public class ValidateInput : IValidateInput
+    {
+        public bool validateUserInput(string username, string password)
+        {
+            return true;
+            //validate code here
+        }
+    }
+
+    public class DatabaseOperations : IDatabaseOperations
+    {
+        public bool SaveInput()
+        {
+            return true;
+            //validate code here
+        }
+    }
+
+    public class EmailService : IEmailService
+    {
+        public void sendEmail()
+        {
+
+        }
+    }
+
+    public class UserManager
+    {
+        private readonly IValidateInput _validateInput;
+        private readonly IDatabaseOperations _databaseOperations;
+        private readonly IEmailService _emailService;
+
+        public UserManager(IValidateInput validateInput, IDatabaseOperations databaseOperations, IEmailService emailService)
+        {
+            _validateInput = validateInput;
+            _databaseOperations = databaseOperations;
+            _emailService = emailService;
+        }
+
+        public void RegisterUser(string username, string password)
+        {
+            _validateInput.validateUserInput(username, password); // Validate input
+            _databaseOperations.SaveInput();  // Save to database
+            _emailService.sendEmail(); // Send welcome email
+        }
+    }
+
+}
